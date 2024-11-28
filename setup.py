c4e055e1a9119027ffc1f02a30deb246b44fd0bc
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-28 20:41:51 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+# First boot - Base setup
+# Link from MD
+# curl -L https://raw.githubusercontent.com/cms66/rpi-pyhome/main/setup.py | sudo python
+# Assumes
+# - rpi imager used to configure
+#    - user/password
+#    - hostname
+#    - WiFi
+# TODO
+# - Check for first run/delete on success
+
+# Imports
+import subprocess, sys, os
+
+# Run commands
+print("Python setup")
+subprocess.run(["wget", "https://raw.githubusercontent.com/cms66/rpi-pyhome/main/base_setup.sh"])
+subprocess.run(["sudo", "bash", "./base_setup.sh"])
+subprocess.run(["sudo", "rm", "-f", "./base_setup.sh"]) # Delete bash script
+usropt=input("Base seup done, press p to poweroff or any other key to reboot: ").lower()
+os.remove(__file__) # Delete python script
+if usropt == 'p':
+    print ("Poweroff selected")
+    subprocess.call(["shutdown", "-s", "now"])
+else:
+    print ("Reboot selected")
+    subprocess.call(["shutdown", "-r", "now"])
