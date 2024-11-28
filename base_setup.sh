1c259f9a6416f87b7edbc92562cf559a84e97f2a
cms66 <58144229+cms66@users.noreply.github.com>
2024-11-28 21:07:17 +0000

    Update base_setup.sh

--- base_setup.sh
+++ base_setup.sh
@@ -1,1 +1,11 @@
 # Base setup bash script
+# Error handling
+set -e
+# Error handler
+handle_error()
+{
+	echo "Error: $(caller) : ${BASH_COMMAND}"
+}
+# Set the error handler to be called when an error occurs
+trap handle_error ERR
+
